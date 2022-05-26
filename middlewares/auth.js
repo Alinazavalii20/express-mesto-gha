cd67f01c2f66090d50e8b2cc1051b506edccf865
Alinazavalii20 <87885095+Alinazavalii20@users.noreply.github.com>
2022-05-26 21:18:37 +0300

    Add files via upload

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,20 +1,21 @@
 const jwt = require('jsonwebtoken');
 const { UnAuthtorizeError } = require('../errors/UnAuthtorizeError');
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const authorization = req.cookies.jwt;
 
   if (!authorization) {
     next(new UnAuthtorizeError('Необходима авторизация'));
   } else {
-    const token = authorization;
     let payload;
+    const token = authorization;
     try {
       payload = jwt.verify(token, 'super-secret-key');
     } catch (err) {
-      next(new UnAuthtorizeError('Необходима авторизация'));
+      return next(new UnAuthtorizeError('Необходима авторизация'));
     }
     req.user = payload;
-    next();
+    return next();
   }
 };
